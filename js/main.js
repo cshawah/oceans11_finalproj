127e4dc9093b7aa3427d7a2aea1385f6986cf2a4
Tito Yak <42939976+TitoYak211@users.noreply.github.com>
2021-11-14 17:19:28 -0500

    - scatterplot

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 console.log("let's get started!")
+
+let scatterplot;
+
+d3.csv("data/recent-grads.csv").then(function (data) {
+    initMainPage(data)
+})
+    .catch(function (err) {
+        console.log(err)
+    });
+
+
+// initMainPage
+function initMainPage(dataArray) {
+
+    console.log('check out the data', dataArray);
+
+    // init scatterplot
+    scatterplot = new ScatterplotVis('gender_salary_bars', dataArray);
+}
