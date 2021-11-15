00e275ef30aff42743def5c05dc8769278bdc490
cshawah <chloeshawah@college.harvard.edu>
2021-11-14 22:24:49 -0500

    prettifying

--- js/scatterplotVis.js
+++ js/scatterplotVis.js
@@ -12,7 +12,8 @@ class ScatterplotVis {
 
         vis.margin = {top: 20, right: 20, bottom: 20, left: 60};
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
-        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
+        vis.height = 500 - vis.margin.top - vis.margin.bottom;
+       // vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
         // init drawing area
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
