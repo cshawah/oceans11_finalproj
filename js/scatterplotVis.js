c836c13065cb2e9a90b58d9b3d5bdbbc04ea32cb
Tito Yak <42939976+TitoYak211@users.noreply.github.com>
2021-12-05 14:36:04 -0500

    Update scatterplotVis.js

--- js/scatterplotVis.js
+++ js/scatterplotVis.js
@@ -15,7 +15,6 @@ class ScatterplotVis {
         vis.margin = {top: 20, right: 20, bottom: 50, left: 60};
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = 500 - vis.margin.top - vis.margin.bottom;
-       // vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
         // init drawing area
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
@@ -24,22 +23,10 @@ class ScatterplotVis {
             .append('g')
             .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
 
-        // add title
-        // vis.svg.append('g')
-        //     .attr('class', 'title bar-title')
-        //     .append('text')
-        //     .text('Gender and Salary Scatter Plot')
-        //     .attr('transform', `translate(${vis.width / 2}, 10)`)
-        //     .attr('text-anchor', 'middle');
-
         // append tooltip
         vis.tooltip = d3.select("body").append('div')
             .attr('class', "tooltip")
             .attr('id', 'circleTooltip')
-
-        // vis.colorScale = d3.scaleQuantize()
-        //     .range(["rgb(237,248,233)", "rgb(186,228,179)",
-        //         "rgb(116,196,118)", "rgb(49,163,84)", "rgb(0,109,44)"])
 
         // D3 color scale.
         vis.categories = new Set()
@@ -47,8 +34,6 @@ class ScatterplotVis {
         for (let i = 0; i< vis.displayData.length; i++) {
             vis.categories.add(vis.displayData[i].Major_category)
         };
-
-        // vis.colors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#8B00FF', '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
         // Scales and axes
         vis.x = d3.scaleLinear()
@@ -155,9 +140,7 @@ class ScatterplotVis {
                                 <h5 style="font-weight: bold">${d.Major}<h5>
                                 <h5>Category: ${d.Major_category}<h5>
                                 <h5>Median Income: $${d.Median.toLocaleString("en-US")}</h5> 
-                                <h5>Percentage of Women: ${Math.floor(d.ShareWomen)}%</h5>         
-                                <!-- <h5>Men: ${d.Men}</h5>            
-                                <h5>Women: ${d.Women}</h5> -->                     
+                                <h5>Percentage of Women: ${Math.floor(d.ShareWomen)}%</h5>                  
                              </div>`);
                 }
             ).on('mouseout', function(){
